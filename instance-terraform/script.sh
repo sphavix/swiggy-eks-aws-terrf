eec23098e5f698bf73683f41f7743a96ba59805f
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-03-20 08:04:29 +0200

    Install awscli script

--- instance-terraform/script.sh
+++ instance-terraform/script.sh
@@ -28,10 +28,10 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
 # Install AWS CLI 
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#sudo apt-get install unzip -y
-#unzip awscliv2.zip
-#sudo ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt-get install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
 
 # Install Node.js 16 and npm
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource-archive-keyring.gpg
