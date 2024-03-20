5d85fb314f1185cbaa8aa6b9491718aacb5bbed5
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-03-20 08:51:05 +0200

    Modified docker install commands

--- instance-terraform/script.sh
+++ instance-terraform/script.sh
@@ -40,8 +40,8 @@ sudo apt update
 sudo apt install -y nodejs
 
 # install docker and run sonarqube conatainer
-#sudo apt-get update
-#sudo apt-get install docker.io -y
+sudo apt-get update
+sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
